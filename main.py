f9322436ff331df2680113cce1bb0987f1697b74
Kevin Lacker <lacker@gmail.com>
2020-02-13 15:57:39 -0800

    checks

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 #!/usr/bin/env python3
 
-print("hello")
+words = [line.strip() for line in open("./words_alpha.txt").readlines()]
 
-lines = open("./words_alpha.txt").readlines()
+print("loaded dictionary with", len(words), "words")
 
-print("loaded dictionary with", len(lines), "words")
+
+def canonicalize(word):
+    letters = set()
+    for letter in word:
+        letters.add(letter)
+    return "".join(sorted(list(letters))) + f" ({len(word)})"
+
+
+cmap = {}
+for word in words:
+    key = canonicalize(word)
+    if key not in cmap:
+        cmap[key] = []
+    cmap[key].append(word)
+
+
+def check(word):
+    can = canonicalize(word)
+    words = cmap.get(can, [])
+
+    if len(words) > 1:
+        print("collision:", words)
+        return
+
+    print(can.upper(), "=>", word)
+
+
+# under bed
+check("buttercup")
+check("snapdragon")
+check("foxglove")
+check("morningglory")
+check("bluebell")
+check("goldenrod")
