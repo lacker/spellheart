0d36cd1bce7dae7beac5ab53fe060fa511d8e3ee
Kevin Lacker <lacker@gmail.com>
2020-02-13 17:06:24 -0800

    phase two ok

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+#
+# A puzzle generator
 
 words = [line.strip() for line in open("./words_alpha.txt").readlines()]
 
@@ -20,21 +22,37 @@ for word in words:
     cmap[key].append(word)
 
 
-def check(word):
+def check(word, ignore_collision=False):
     can = canonicalize(word)
     words = cmap.get(can, [])
 
-    if len(words) > 1:
+    if not ignore_collision and len(words) > 1:
         print("collision:", words)
         return
 
     print(can.upper(), "=>", word)
 
 
+# With the puzzles printed out, you can use a letter multiple times.
+# Write out the blanks and put a circle where the clue-letter or clue-letters go
+
 # under bed
+# theme: flowers
 check("buttercup")
 check("snapdragon")
 check("foxglove")
 check("morningglory")
 check("bluebell")
 check("goldenrod")
+
+# colander
+# theme: trees
+check("sycamore", True)
+check("redwood", True)
+check("willow")
+check("manzanita")
+check("cedar", True)
+check("eucalyptus")
+check("redbud", True)
+
+# dustpan
